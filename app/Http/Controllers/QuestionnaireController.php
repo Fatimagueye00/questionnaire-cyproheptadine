c91272f0d71c336889bcda17147975cc2ef3da0e
Fatimagueye00 <gfatoumata121@gmail.com>
2026-09-02 17:27:35 +0000

    Correction ordre affichage réponses

--- app/Http/Controllers/QuestionnaireController.php
+++ app/Http/Controllers/QuestionnaireController.php
@@ -1,32 +1,44 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use App\Models\Question;
 use App\Models\QuestionnaireResponse;
 use Illuminate\Http\Request;
+
 class QuestionnaireController extends Controller
 {
     public function index()
     {
-        $questions = Question::with('answers')
-            ->where('active', true)
-            ->orderBy('order', 'asc')
-            ->get();
+        $questions = Question::with([
+            'answers' => function ($query) {
+                $query->orderBy('order', 'asc');
+            }
+        ])
+        ->where('active', true)
+        ->orderBy('order', 'asc')
+        ->get();
+
         foreach ($questions as $question) {
             $question->setRelation(
                 'answers',
                 $question->answers->unique('id')->values()
             );
         }
+
         return view('questionnaire.index', compact('questions'));
     }
+
     public function submit(Request $request)
     {
         $reponses = $request->except([
             '_token',
         ]);
+
         QuestionnaireResponse::create([
             'reponses' => $reponses,
         ]);
+
         return redirect()
             ->route('questionnaire')
             ->with(
